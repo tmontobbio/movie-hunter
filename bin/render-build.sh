ec54a85b6be3e77851fde69b05d2fe2bf74437ff
Tyler <toygrrr@gmail.com>
2022-12-01 15:49:34 -0800

    fixed render file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,11 @@
 # exit on error
 set -o errexit
 
+# Build commands for front end to create the production build
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+# Build commands for back end
 bundle install
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
-bundle exec rake db:create
-bundle exec rake db:migrate
+bundle exec rake db:migrate 
